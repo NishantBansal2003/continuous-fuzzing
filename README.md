e305c0c91eeb907366207fe82cf610c4a6720a38
Nishant Bansal <nishant.bansal.282003@gmail.com>
2025-03-22 18:11:17 +0530

    add support for clutserfuzzlite
    Signed-off-by: Nishant Bansal <nishant.bansal.282003@gmail.com>

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ To Run the Fuzz test use:
 ```sh
 go test -fuzz=FuzzParseString ./fuzzing -test.fuzzcachedir="../"
 ```
+
+## Fuzzing using ClutserFuzzLite
+```sh
+python ../oss-fuzz/infra/helper.py build_image --external $PATH_TO_PROJECT
+python ../oss-fuzz/infra/helper.py build_fuzzers --external $PATH_TO_PROJECT --sanitizer address
+```
---
+++ fuzz/fuzz.go
@@ -0,0 +1,18 @@
+package fuzz
+
+import "fuzzing/fuzzing"
+
+func Fuzz(data []byte) int {
+	v := string(data)
+
+	// As per the documentation, this isn't always equal to v, so it makes
+	// no sense to check for equality. It can still be interesting to find
+	// panics in it though.
+	err := fuzzing.ParseString(v)
+
+	if err != nil {
+		return 1
+	}
+
+	return 0
+}
