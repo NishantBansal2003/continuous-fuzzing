558e1a1db372e79f21d49a8be891a904f1189e55
Nishant Bansal <nishant.bansal.282003@gmail.com>
2025-03-24 12:25:23 +0530

    add readme for coverage fuzzing
    Signed-off-by: Nishant Bansal <nishant.bansal.282003@gmail.com>

--- go-native-fuzzing/parser_test.go
+++ go-native-fuzzing/parser_test.go
@@ -7,6 +7,8 @@ import (
 func FuzzParseString(f *testing.F) {
 	// f.Add("1, Name, 23rd Fifth Street")
 	// f.Add("2, Name2, 1 Main street")
+	//! corpusDir = "testdata/fuzz" this is found in fuzz std directory
+	//! so the fuzz corpus is feed from f.Add and corpusDir
 	f.Fuzz(func(t *testing.T, s string) {
 		err := ParseString(s)
 		if err != nil {
